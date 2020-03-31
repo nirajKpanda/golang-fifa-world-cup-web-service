6c3201140b3f74baa08b675eab284ad0ef536f69
nirajKpanda <nirajpanda@outlook.com>
2020-03-31 12:50:52 +0530

    Inital commits

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -6,11 +6,33 @@ import (
 
 // RootHandler returns an empty body status code
 func RootHandler(res http.ResponseWriter, req *http.Request) {
-
+	res.WriteHeader(http.StatusNoContent)
+	return
 }
 
 // ListWinners returns winners from the list
 func ListWinners(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	year := req.URL.Query().Get()
+	
+	winners, err := data.ListAllJSON()
+	
+	if err != nil {
+		res.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	
+	if year == ""{
+		res.Write(winners)
+	} else{
+		filteredWinners, err := data.ListAllByYear()
+		if err != nil{
+			res.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		res.Write(filteredWinners)
+		return
+	}
 
 }
 
